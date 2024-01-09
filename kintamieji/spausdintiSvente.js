d55b7e01b57fd52c7c61c8486cedb497d9fee099
Aina <ainaeinikyte@gmail.com>
2024-01-09 11:32:31 +0200

    Import function document

---
+++ kintamieji/spausdintiSvente.js
@@ -0,0 +1,17 @@
+function spausdintiSvente(index, holiday) {
+    const name = holiday.name;
+    const year = holiday.date.year;
+    const month = holiday.date.month;
+    const day = holiday.date.day;
+    const monthNames = [
+        'Sausio', 'Vasario', 'Kovo', 'Balandzio',
+        'Geguzes', 'Birzelio', 'Liepos', 'Rugpjucio',
+        'Rugsejo', 'Spalio', 'Lapkricio', 'Gruodzio',
+    ];
+
+    const monthName = monthNames[month - 1];
+        //holiday.name  holiday.date.year
+    return `${index + 1} ${name} - ${year}m. - ${monthName} - ${day}d.`;
+}
+
+export default spausdintiSvente;
